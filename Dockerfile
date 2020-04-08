7ff0384825a4088ae6c397c874a5d7da275fc7b5
lucus lee <lixin9311@gmail.com>
2020-04-08 07:58:02 +0000

    Accept json array as argument

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM alpine
 LABEL maintainer="lucas.li@linktivity.co.jp"
-RUN apk --no-cache add ca-certificates wget && update-ca-certificates
+RUN apk --no-cache add ca-certificates jq wget && update-ca-certificates
 RUN wget -O /usr/local/bin/kustomize https://github.com/kubernetes-sigs/kustomize/releases/download/v2.0.3/kustomize_2.0.3_linux_amd64
 RUN chmod +x /usr/local/bin/kustomize
-ENTRYPOINT ["/bin/sh", "-c"]
+COPY entrypoint.sh /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -l
+
+# TAG_NAME or SHORT_SHA
+# PROJECT_ID
+# ENVIRONMENT
+# SVC_PREFIX
+
+kustomize_edit_build() {
+    local service=$1
+    cd $SVC_PREFIX &&
+    ls &&
+    cd $service &&
+
+    cd k8s/$ENVIRONMENT &&
+    echo 'working in' $(pwd) &&
+    kustomize edit set image $service=gcr.io/$PROJECT_ID/$service:$image_tag > /dev/null 2>&1 &&
+    kustomize build > $dist/$service.yaml &&
+    echo "$service manifest generated"
+    cd $workspace
+}
+
+image_tag=""
+dist=$(pwd)/dist
+workspace=$(pwd)
+
+mkdir -p $dist
+
+if [ -z "$TAG_NAME" ]; then
+    image_tag="$SHORT_SHA"
+else
+    image_tag="$TAG_NAME"
+fi
+
+echo "arguments: $1"
+
+for var in $(jq -r '.[]' <<< "$1"); do
+    kustomize_edit_build $var
+    result=$?
+    if [ ! $result -eq 0 ]; then
+        echo "error: $result"
+        exit $result
+    fi
+done
